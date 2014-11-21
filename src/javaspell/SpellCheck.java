34cbf684ce6d7185f70bec2afd02823039276975
aa <shameeonline@gmail.com>
2014-11-21 14:46:56 -0800

    Code changed

--- src/javaspell/SpellCheck.java
+++ src/javaspell/SpellCheck.java
@@ -1,7 +1,9 @@
 package javaspell;
 
 import static org.junit.Assert.assertEquals;
+import pt.tumba.spell.DefaultWordFinder;
 import pt.tumba.spell.SpellChecker;
+import pt.tumba.spell.TeXWordFinder;
 
 public class SpellCheck {
 
@@ -10,14 +12,26 @@ public class SpellCheck {
 		 SpellChecker spellCheck = new SpellChecker();
 	       // spellCheck.initialize ("dict/english.txt.gz", "dict/common-misspells.txt.gz", "dict/jargon.txt.gz");
 	     try {
-			spellCheck.initialize ("C:/code/workspace-trunk-cstool/javaspell-master/src/english.txt.gz");
+/*			spellCheck.initialize ("C:/code/workspace-trunk-cstool/javaspell-master/src/english.txt.gz");
 			String word = spellCheck.findMostSimilar ("hello");
 	        assertEquals ("hello", word);
-	        System.out.println(word);
+	        System.out.println(word);*/
+	        
+	  	    searchWord("1murugan of shamee string 1mango","1m");   
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	private static void searchWord(String testText, String expected){ 	  
+		    TeXWordFinder finder = new TeXWordFinder(testText);  
+		    while(finder.hasNext()){
+			    String found = finder.next();   
+			    if(found.contains(expected)){
+			    	System.out.println(found);	 
+			    }
+		    }
+	 } 
 
 }
